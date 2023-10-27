ea34aec3629a5c01dacb961d44b61f219fd403fd
andre.silva <andre.silva@protendit.com.br>
2023-10-27 15:07:38 -0300

    update in paths

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -10,14 +10,15 @@ import { Category } from "./category.model";
   providedIn: 'root'
 })
 
-// Define uma classe TypeScript chamada CategoryService e a torna disponível para exportação para que ela possa ser usada em outros módulos.
+// // Define uma classe TypeScript chamada CategoryService e a torna disponível para exportação para que ela possa ser usada em outros módulos.
 export class CategoryService {
 
   // Isso declara uma variável de membro privada chamada apiPath que armazena o caminho base da API relacionado às categorias. Isso é útil para construir URLs de solicitação HTTP.
-  private apiPath: string = "api/categories"
+  private apiPath: string = "api/categories";
 
   // O construtor da classe CategoryService recebe uma injeção de dependência do serviço HttpClient. Isso permite que o serviço faça solicitações HTTP para a API.
   constructor(private http: HttpClient) { }
+
 
   // Este método é responsável por recuperar todas as categorias da API. Ele retorna um Observable que emite uma matriz de objetos Category. O método utiliza o serviço HttpClient para fazer uma solicitação GET à URL apiPath e, em seguida, manipula a resposta usando os operadores catchError e map para lidar com erros e transformar os dados da resposta em objetos Category.
   getAll(): Observable<Category[]> {
@@ -58,14 +59,17 @@ export class CategoryService {
   // Este método é usado para excluir uma categoria com base em seu ID. Ele retorna um Observable que não emite um valor útil, apenas confirma que a exclusão foi bem-sucedida.
   delete(id: number): Observable<any> {
     const url = `${this.apiPath}/${id}`;
-    
+
     return this.http.delete(url).pipe(
       catchError(this.handleError),
       map(() => null)
     )
   }
 
+
+
   // PRIVATE METHODS
+
 
   // Este método privado é usado para converter os dados recebidos da API em uma matriz de objetos Category.
   private jsonDataToCategories(jsonData: any[]): Category[] {
@@ -75,7 +79,7 @@ export class CategoryService {
   }
 
   // Este método privado converte os dados de uma categoria específica em um objeto Category.
-  private jsonDataCategory(jsonData: any): Category {
+  private jsonDataToCategory(jsonData: any): Category {
     return jsonData as Category;
   }
 
@@ -84,5 +88,4 @@ export class CategoryService {
     console.log("ERRO NA REQUISIÇÃO => ", error);
     return throwError(error);
   }
-
 }
