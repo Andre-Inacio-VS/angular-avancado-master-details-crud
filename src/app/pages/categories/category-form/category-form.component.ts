675f12738852288cf598f4f24e7dc15a9a4e8400
andre.silva <andre.silva@protendit.com.br>
2023-10-31 10:12:42 -0300

    improving CategoryFormComponent

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -29,7 +29,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked{
     private formBuilder: FormBuilder
   ) { }
 
-  ngOnInit() {
+  ngOnInit(){
     this.setCurrentAction();
     this.buildCategoryForm();
     this.loadCategory();
@@ -37,6 +37,16 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked{
 
   ngAfterContentChecked(){
     this.setPageTitle();
+  }
+
+  submitForm(){
+    this.submittingForm = true;
+
+    if(this.currentAction == "new"){
+      this.createCategory();
+    } else {
+      this.updateCategory();
+    }
   }
 
   // PRIVATE METHODS
@@ -78,4 +88,45 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked{
       this.pageTitle = "Editando Categoria: " + categoryName;
     }
   }
+
+  private createCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      )
+  }
+
+  private updateCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.update(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      )
+  }
+
+  private actionsForSuccess(category: Category){
+    toastr.success("Solicitação processada com sucesso!");
+
+    // redirect/reload component page
+    this.router.navigateByUrl("categories", {skipLocationChange: true}).then(
+      () => this.router.navigate(["categories", category.id, "edit"])
+    )
+  }
+
+  private actionsForError(error: { status: number; _body: string; }){
+    toastr.error("Ocorreu um erro ao processa sua solicitação!");
+
+    this.submittingForm = false;
+
+    if(error.status === 422)
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    else  
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente novamente mais tarde."]
+  }
+
 }
