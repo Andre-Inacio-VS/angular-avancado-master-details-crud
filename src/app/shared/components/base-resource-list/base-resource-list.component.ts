3885c7fe4b26acddbfdcffbdcb2689cec6502f68
andre.silva <andre.silva@protendit.com.br>
2023-11-03 11:50:16 -0300

    improving EntryList with BaseResourceListComponent

--- src/app/shared/components/base-resource-list/base-resource-list.component.ts
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -5,7 +5,7 @@ import { BaseResourceService } from "../../services/base-resource.service";
 
 export abstract class BaseResourceListComponent<T extends BaseResourceModel> implements OnInit {
 
-  resources : T[] = [];
+  resources: T[] = [];
 
   constructor(private resourceService: BaseResourceService<T>) { }
 
@@ -17,7 +17,7 @@ export abstract class BaseResourceListComponent<T extends BaseResourceModel> imp
   }
 
   deleteResource(resource: T) {
-    const mustDelete = confirm('Deseja realmente excluir este item?')
+    const mustDelete = confirm('Deseja realmente excluir este item?');
 
     if(mustDelete){
       this.resourceService.delete(resource.id).subscribe(
